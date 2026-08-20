83b4afba025ccb846103aeb66c4731fa0be6a5f8
tomaszmilewski1 <tomasz_milewski1@outlook.com>
2026-08-20 18:57:14 +0200

    Update Recipes.tsx

--- components/Recipes.tsx
+++ components/Recipes.tsx
@@ -2790,9 +2790,12 @@ export default function Recipes() {
                           Koszt
                         </span>
 
-<strong>
+                        <strong>
   {formatMoney(
-    Number(recipe.cost ?? 0)
+    Number(recipe.cost ?? 0) +
+      Number(recipe.labor_cost ?? 0) +
+      Number(recipe.energy_cost ?? 0) +
+      Number(recipe.packaging_cost ?? 0)
   )}
 </strong>
                       </div>
